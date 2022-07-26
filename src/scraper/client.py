adddde79ed9e2f1c657da1d8fff113102eabff83
RakNV <kinglink852@gmail.com>
2022-07-26 16:56:21 +0300

    Moved cfg from client to config.py

--- src/scraper/client.py
+++ src/scraper/client.py
@@ -1,21 +1,18 @@
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty
+from src.scraper.config import phone, api_id, api_hash
 
-api_id = 15741274
-api_hash = '551169796fc89f6dfc844e0046b197c9'
-phone = '+380982159820'
-message = 'Здравствуй, я тестирую прогу, сорри за сообщение'
 
-clientl = TelegramClient(phone, api_id, api_hash)
+client = TelegramClient(phone, api_id, api_hash)
 
 # authorization happens here
-clientl.connect()
-if not clientl.is_user_authorized():
-    clientl.send_code_request(phone)
-    clientl.sign_in(phone, input('Enter the code: '))
+client.connect()
+if not client.is_user_authorized():
+    client.send_code_request(phone)
+    client.sign_in(phone, input('Enter the code: '))
 
-result = clientl(GetDialogsRequest(
+result = client(GetDialogsRequest(
     offset_date=None,  # last_date = None
     offset_id=0,
     offset_peer=InputPeerEmpty(),
