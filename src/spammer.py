75c9d78f9fdf8b99e360780ffb355332e37a7458
RakNV <kinglink852@gmail.com>
2022-07-26 16:56:58 +0300

    small changes in imports

--- src/spammer.py
+++ src/spammer.py
@@ -1,6 +1,7 @@
-from scraper.client import clientl, message
+from scraper.client import client
 from telethon.tl.types import InputPeerUser
 from telethon.errors.rpcerrorlist import PeerFloodError
+from src.scraper.config import message, sleep_time
 import time
 import sys
 
@@ -8,17 +9,16 @@ import sys
 class Spammer:
 
     def message_users(self, users):
-        sleep_time = 5
         for user in users:
             receiver = InputPeerUser(user["id"], user["access_hash"])
             try:
                 print("Sending Message to:", user["name"])
-                clientl.send_message(receiver, message.format(user["name"]))
+                client.send_message(receiver, message.format(user["name"]))
                 print("Waiting {} seconds".format(sleep_time))
                 time.sleep(sleep_time)
             except PeerFloodError:
                 print("Getting Flood Error from telegram. Script is stopping now. Please try again after some time.")
-                clientl.disconnect()
+                client.disconnect()
                 sys.exit()
             except Exception as e:
                 print("Error:", e)
