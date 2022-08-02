74f6837484104febd4e97c94300e10c5857dddd5
RakNV <kinglink852@gmail.com>
2022-08-02 04:03:51 +0300

    keyboard interupt handling + saving messaged users

--- src/main.py
+++ src/main.py
@@ -1,14 +1,24 @@
 from scraper.parser import Parser
 from spammer import Spammer
+from scraper.client import client
 
 if __name__ == "__main__":
-    ps = Parser()
-    sp = Spammer()
-    ps.print_groups()
-    ps.choose_group()
-    ps.get_participants()
-    ps.save_in_csv()
-    ps.csv_to_list()
-    print(ps.users)
-    sp.message_users(ps.users)
+    try:
+        ps = Parser()
+        sp = Spammer()
+        ps.print_groups()
+        ps.choose_group()
+        ps.get_participants()
+        ps.save_in_csv()
+        ps.csv_to_list()
+        sp.message_all_users(ps.users)
+        print("\n\nSaving messaged users...")
+        Spammer.save_messaged_user(Spammer.messaged_users)
+        print("Exiting...")
+    except KeyboardInterrupt:
+        print("\n\nSaving messaged users...")
+        Spammer.save_messaged_user(Spammer.messaged_users)
+        print("Exiting...")
+        client.disconnect()
+        exit()
 
