4991d1c3506fa7de49f2df0b6cc6c705f45db9f8
RakNV <kinglink852@gmail.com>
2022-09-10 05:52:16 +0300

    added __init__, get_group, get_all_participants, moved is_messaged here

--- src/spammer.py
+++ src/spammer.py
@@ -1,9 +1,7 @@
-from scraper.client import authorized_clients
 from telethon.tl.types import InputPeerUser
 from telethon.errors.rpcerrorlist import PeerFloodError, FloodWaitError
 from src.scraper.config import message, sleep_time
 import time
-import sys
 import re
 import csv
 
@@ -11,16 +9,85 @@ import csv
 class Spammer:
     messaged_users = []
 
+    def __init__(self, client):
+        self.client = client
+        self.target_group = None
+        self.all_participants = None
+
+    def get_group(self, group_list, target_group_name):
+        for group in group_list:
+            if group.title == target_group_name:
+                self.target_group = group
+
+    def get_all_participants(self):
+        """gets participants data from group,chat,channel, ect."""
+        print('Fetching Members...')
+        self.all_participants = self.client.tlclient.get_participants(self.target_group)
+        users = []
+        for user_data in self.all_participants:
+            if user_data.username:
+                username = user_data.username
+            else:
+                username = ""
+            if user_data.first_name:
+                first_name = user_data.first_name
+            else:
+                first_name = ""
+            if user_data.last_name:
+                last_name = user_data.last_name
+            else:
+                last_name = ""
+            name = (first_name + ' ' + last_name).strip()
+            user = {
+                "username": username,
+                "id": user_data.id,
+                "access_hash": user_data.access_hash,
+                "name": name
+            }
+            users.append(user)
+        self.all_participants = users
+
+    def message_45_users(self, users):
+        Spammer.load_messaged_users()
+        counter = 0
+        for user in users:
+            if counter == 2:
+                break
+            receiver = InputPeerUser(user["id"], user["access_hash"])
+            try:
+                if Spammer.is_messaged(user):
+                    continue
+                else:
+                    self.message_user(user, receiver) # have to change THIS line of code authorized clients[index] change  to client argument in function
+                    counter += 1
+
+            except PeerFloodError:
+                print("Getting Flood Error from telegram. Script is stopping now. Please try again after some time.")
+                self.client.disconnect()
+                exit(1)
+
+            except FloodWaitError as e:
+                Spammer.wait_flood(e)
+                Spammer.message_user(user, receiver)
+
+    @staticmethod
+    def is_messaged(user):
+        ids = [messaged_user["id"] for messaged_user in Spammer.messaged_users]
+        if user["id"] in ids:
+            return True
+        else:
+            return False
+
     @staticmethod
     def save_messaged_user(users):
         with open("members_messaged.csv", "w", encoding='UTF-8') as f:
             writer = csv.writer(f, delimiter=",", lineterminator="\n")
-            writer.writerow(['username', 'user id', 'access hash', 'name'])
+            writer.writerow(['username', 'user_id', 'access_hash', 'name'])
             for user in users:
-                writer.writerow([user["username"],
-                                 user["id"],
-                                 user["access_hash"],
-                                 user["name"]])
+                writer.writerow([user['username'],
+                                 user['id'],
+                                 user['access_hash'],
+                                 user['name']])
 
     @staticmethod
     def load_messaged_users():
@@ -32,19 +99,14 @@ class Spammer:
                         'id': int(row[1]),
                         'access_hash': int(row[2]),
                         'name': row[3]}
-                Spammer.messaged_users.append(user)
+                if user in Spammer.messaged_users:
+                    continue
+                else:
+                    Spammer.messaged_users.append(user)
 
-    @staticmethod
-    def is_messaged(user):
-        if user in Spammer.messaged_users:
-            return True
-        else:
-            return False
-
-    @staticmethod
-    def message_user(client, user, receiver):
+    def message_user(self, user, receiver):
         print("Sending Message to:", user["name"])
-        client.send_message(receiver, message.format(user["name"]))
+        self.client.tlclient.send_message(receiver, message)
         Spammer.messaged_users.append(user)
         print(user)
         print("Waiting {} seconds".format(sleep_time))
@@ -58,28 +120,4 @@ class Spammer:
         print(f"Waiting {wait_time} seconds")
         time.sleep(wait_time)
 
-    def message_all_users(self, users):
-        Spammer.load_messaged_users()
-        for user in users:
-            receiver = InputPeerUser(user["id"], user["access_hash"])
-            try:
-                if Spammer.is_messaged(user):
-                    continue
-                else:
-                    Spammer.message_user(authorized_clients[1],user, receiver)
 
-            except PeerFloodError:
-                print("Getting Flood Error from telegram. Script is stopping now. Please try again after some time.")
-                for i in range(0,len(authorized_clients)):
-                    authorized_clients[i].disconnect()
-                sys.exit()
-
-            except FloodWaitError as e:
-                Spammer.wait_flood(e)
-                Spammer.message_user(user, receiver)
-                Spammer.messaged_users.append(user)
-
-            except Exception as e:
-                print("Error:", e)
-                print("Trying to continue...")
-                continue
